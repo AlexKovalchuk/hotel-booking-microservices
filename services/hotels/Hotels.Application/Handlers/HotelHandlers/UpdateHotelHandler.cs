7ed43c992933c3b545deb7fb6514cebf4cf08d4d
Oleksandr Kovalchuk <oleksandr.kovalchuk@avenga.com>
2026-05-14 16:35:03 +0200

    Block 3: Phase 5: rename Rooms Controller

--- services/hotels/Hotels.Application/Handlers/HotelHandlers/UpdateHotelHandler.cs
+++ services/hotels/Hotels.Application/Handlers/HotelHandlers/UpdateHotelHandler.cs
@@ -1,8 +1,7 @@
 using Hotels.Application.Abstractions;
 using Hotels.Application.DTOs.Hotel;
-using Hotels.Domain.Entities;
 
-namespace Hotels.Application.Handlers;
+namespace Hotels.Application.Handlers.HotelHandlers;
 
 public class UpdateHotelHandler(IHotelRepository hotelRepository)
 {
